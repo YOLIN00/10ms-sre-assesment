c1917a5a9b9e12843954f998b69903a59dd5a36f
YOLIN00 <ibn20172016@gmail.com>
2025-11-26 11:42:36 +0600

    ops-test-2 application issue fixed

--- ops-test-2/app/server.go
+++ ops-test-2/app/server.go
@@ -5,30 +5,32 @@ import (
 	"log"
 	"net/http"
 	"sync"
-	"time"
+	// "time"
 )
 
 var counter int
 var lock sync.Mutex
-var leak = []string{}
+// var leak = []string{}
+
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	lock.Lock()
 	counter++
+	currCounter := counter 
 	lock.Unlock()
 
-	time.Sleep(2 * time.Second)
+	// time.Sleep(2 * time.Second)
 
-	leak = append(leak, fmt.Sprintf("req-%d", counter))
+	// leak = append(leak, fmt.Sprintf("req-%d", counter))
 
-	fmt.Fprintf(w, "counter=%d", counter)
+	fmt.Fprintf(w, "counter=%d", currCounter)
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
-	if time.Now().Unix()%2 == 0 {
-		w.WriteHeader(500)
-		return
-	}
+	// if time.Now().Unix()%2 == 0 {
+	// 	w.WriteHeader(500)
+	// 	return
+	// }
 	w.Write([]byte("ok"))
 }
 
@@ -37,6 +39,6 @@ func main() {
 	http.HandleFunc("/healthz", health)
 
 	log.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
